93b95d38f3d94dea99e29dcefaf781135f475b80
mcz9mm <k.m.swim411@gmail.com>
2019-12-25 18:03:40 +0900

    step3 create statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Image(
+          image: AssetImage('images/ball1.png'),
+        ),
+      ),
+    );
+  }
+}
